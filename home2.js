6e58537fdde186571149c7a97335b4b0a3310609
Casiana Mba Maye <cmbamaye@students.kennesaw.edu>
2020-03-26 13:54:58 -0400

    stuff

---
+++ home2.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $("#jsstyle").click(function() {
+        $("#img-container").empty();
+        $.getJSON("home2.json", function(data) {
+            $.each(data, function(key, val) {
+                if(key === "pics") {
+                    $.each(val, function(index, value) {
+                        $("#img-container").append("<img src=\"" + value.url + "\"/>");
+                    });
+                }
+            });
+        });
+    });
+
+    $("#pdf-btn").click(function() {
+        $("#pdf-container").empty();
+        $.getJSON("home2.json", function(data) {
+            $.each(data, function(key, val) {
+                if(key === "pdfs") {
+                    $.each(val, function(index, value) {
+                        $("#pdf-container").append("<a href=\"" + value.url + "\">PDF " + (index+1) + "</a>");
+                    });
+                }
+            });
+        });
+    });
+    
+
+});
